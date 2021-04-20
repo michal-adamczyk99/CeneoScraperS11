45cfd3232257de71f48ffb44d1fb61e81ac72b62
michal-adamczyk99 <s218180@student.uek.krakow.pl>
2021-04-20 15:16:28 +0200

    2021-04-20 MA

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # CeneoScraperS11
 ## Stage 1 - extraction of all components for single opinion 
-1. extraction of single web page content
-2. analisys of single opinion structure 
+1. Extraction of a single web page content
+2. Analisys of a single opinion structure 
 |Component|CSS selector|Variable name|Data type|
 |---------|------------|-------------|---------|
 |Opinion|div.user-post_card|opinion||
@@ -10,16 +10,24 @@
 |Recommendation|span.user-post__author-recomendation > em|recommendation|bool|
 |Stars rating|span.user-post__score-count|stars|float|
 |Content|div.user-post__text|content||
-|Advantages|div.review-feature__col:has(> div[class$="positives"]) > div.review-feature__item|pros||list (str)|
+|Advantages|div.review-feature__col:has(> div[class$="positives"]) > div.review-feature__item|pros|list (str)|
 |Disadvantages|div.review-feature__col:has(> div[class$="negatives"]) > div.review-feature__item|cons|list(str)|
 |Verification|div.review-pz|verified|bool|
 |Post date|span.user-post__published > time:nth-child(1)["datetime"]|post_date|str|
 |Purchase date|span.user-post__published > time:nth-child(2)["datetime"]|purchase_date|str|
 |Usefulness count|span[id^="votes-yes"]|usefulness|int|
 |Uselessness count|span[id^="votes-no"]|uselessness|int|
-3. extraction of single opinion components
-4. transformation of extracted data to given data types
+3. Extraction of single opinion components
+4. Transformation of extracted data to given data types
 
 ## Stage 2 - extraction of all opinions from single page
+1. Definition of a dictionary to store all of the opinion's components
+2. Definition of a list to store dictionaries with opinions
+3. Implementation of loop traversing thorugh all opinions from a single page
 
 ## Stage 3 - extraction of all opinions from single product
+1. Implementation of loop traversing through consecutve pages with opinions
+2. Loading extracted opinions to .json file
+3. Parameterization of a product id 
+
+## Stage 4 - code refactoring
--- main.py
+++ main.py
@@ -2,66 +2,69 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+def get_component(opinion, selector, attribute=None, return_list=False):
+    try:
+        if attribute:
+            return opinion.select(selector).pop(0)[attribute].strip()
+        if return_list:
+            return [item.get_text().strip() for item in opinion.select(selector)] 
+        return opinion.select(selector).pop(0).get_text().strip()
+    except IndexError:
+        return None 
+
+selectors = {
+    "author": ["span.user-post__author-name"],
+    "recommendation": ["span.user-post__author-recomendation > em"],
+    "stars": ["span.user-post__score-count"],
+    "content": ["div.user-post__text"],
+    "pros": ["div.review-feature__col:has(> div[class$=\"positives\"]) > div.review-feature__item",
+             None,
+             True],
+    "cons": ["div.review-feature__col:has(> div[class$=\"negatives\"]) > div.review-feature__item",
+             None,
+             True],
+    "verfied": ["div.review-pz"],
+    "post_date": ["span.user-post__published > time:nth-child(1)", "datetime"],
+    "purchase_date": ["span.user-post__published > time:nth-child(2)", "datetime"],
+    "usefulness": ["span[id^='votes-yes']"],
+    "uselessness": ["span[id^='votes-no']"]
+}
+
 all_opinions = []
 page = 1
+product_id = input("Enter a product ID:")
 
 while True:
     print(page)
 
-    response = requests.get(f"https://www.ceneo.pl/91715703/opinie-{page}", allow_redirects=False)
+    response = requests.get(f"https://www.ceneo.pl/{product_id}/opinie-{page}", allow_redirects=False)
     if response.status_code==200: 
         page_dom = BeautifulSoup(response.text, 'html.parser')
         opinions = page_dom.select("div.js_product-review")
-
-
-
         for opinion in opinions:
+                single_opinion = {key:get_component(opinion,*value)
+                                for key, value in selectors.items()}
+                single_opinion["opinion_id"] = opinion["data-entry-id"]
+                
+                single_opinion["recommendation"] = True if single_opinion[
+                    "recommendation"] == "Polecam" else False if single_opinion["recommendation"] == "Nie polecam" else None
+                single_opinion["stars"] = float(
+                    single_opinion["stars"].split("/")[0].replace(", ", "."))
+                single_opinion["content"] = re.sub(
+                    "\\s", " ", single_opinion["content"])
+                single_opinion["verfied"] = bool(single_opinion["verfied"])
+                single_opinion["usefulness"] = int(single_opinion["usefulness"])
+                single_opinion["uselessness"] = int(single_opinion["uselessness"])
 
-            opinion_id = opinion["data-entry-id"]
-            author = opinion.select("span.user-post__author-name").pop(0).get_text().strip()
-            try:
-                recommendation = opinion.select("span.user-post__author-recomendation > em").pop(0).get_text().strip()
-                recommendation = True if recommendation=="Polecam" else False 
-            except IndexError:
-                recommendation = None
-            stars = opinion.select("span.user-post__score-count").pop(0).get_text().strip()
-            stars = float(stars.split("/")[0].replace(",","."))
-            content = opinion.select("div.user-post__text").pop(0).get_text().strip()
-            content = content.replace("\n", " ").replace("\r", " ")
-            pros = opinion.select("div.review-feature__col:has(> div[class$=\"positives\"]) > div.review-feature__item")
-            pros = [item.get_text().strip() for item in pros]
-            cons = opinion.select("div.review-feature__col:has(> div[class$=\"negatives\"]) > div.review-feature__item")
-            cons = [item.get_text().strip() for item in cons]
-            try:
-                verified = bool(opinion.select("div.review-pz").pop(0).get_text().strip())
-            except IndexError:
-                verified = False
-            post_date = opinion.select("span.user-post__published > time:nth-child(1)").pop(0)["datetime"].strip()
-            try:
-                purchase_date = opinion.select("span.user-post__published > time:nth-child(2)").pop(0)["datetime"].strip()
-            except IndexError:
-                purchase_date = None
-            usefulness = opinion.select("span[id^=\"votes-yes\"]").pop(0).get_text().strip()
-            uselessness = opinion.select("span[id^=\"votes-no\"]").pop(0).get_text().strip()
-
-            single_opinion = {
-                "opinion_id": opinion_id,
-                "author": author,
-                "recommendation": recommendation,
-                "stars": stars,
-                "content": content,
-                "pros": pros,
-                "cons": cons,
-                "verified": verified,
-                "post_date": post_date,
-                "purchase_date": purchase_date,
-                "usefulness": usefulness,
-                "uselessness": uselessness
-            }
-
-            all_opinions.append(single_opinion)
+                all_opinions.append(single_opinion)
         page += 1
     else: break
+
+with open(f"./opinions/{product_id}.json", "w", encoding="UTF-8") as jf:
+    json.dump(all_opinions, jf, ensure_ascii=False, indent=4)
+
+
+
 
 #print(json.dumps(all_opinions, ensure_ascii=False, indent=4))
 
